a9a16eedee110700f76b0ea507e5c1edbb92fa76
Koraiche <80173831+Koraiche@users.noreply.github.com>
2021-05-14 17:32:02 +0000

    Update ec_from_pub.c

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,35 +1,34 @@
 #include "hblk_crypto.h"
 
-
+/**
+ * ec_from_pub - create an EC_KEY structure from a public key
+ * @pub: public key to be converted
+ *
+ * Return: pointer to created EC_KEY, NULL on error
+ */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
-	EC_KEY *key;
-	EC_POINT *pkey;
-	EC_GROUP *group;
-
-	if (!pub)
-		return NULL;
-	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	if (!key)
-		return NULL;
-	group = EC_GROUP_new_by_curve_name(EC_CURVE);
-	if (!group)
-		return NULL;
-	pkey = EC_POINT_new(group);
-	if (!pkey)
-	{
-		EC_GROUP_free(group);
-		return NULL;
-	}
-	if (!EC_POINT_oct2point(group, pkey, pub, EC_PUB_LEN, NULL))
-		return NULL;
-	if (!EC_KEY_set_public_key(key, pkey))
-	{
-		EC_POINT_free(pkey);
-		EC_GROUP_free(group);
-		return NULL;
-	}
-	EC_POINT_free(pkey);
-	EC_GROUP_free(group);
-	return key;
+EC_KEY *key;
+EC_GROUP *group;
+EC_POINT *p;
+if (!pub)
+return (NULL);
+key = EC_KEY_new_by_curve_name(EC_CURVE);
+if (!key)
+return (NULL);
+group = (EC_GROUP *)EC_KEY_get0_group(key);
+if (!group)
+return (NULL);
+p = EC_POINT_new(group);
+if (!p)
+return (NULL);
+if (!EC_POINT_oct2point(group, p, pub, EC_PUB_LEN, NULL)
+|| !EC_KEY_set_public_key(key, p))
+{
+EC_KEY_free(key);
+EC_POINT_free(p);
+return (NULL);
+}
+EC_POINT_free(p);
+return (key);
 }
